06f270654a2862f0738af66055a3d2b4994eae92
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-03-29 13:24:07 +0900

    ✨feat : RedisConfig.java 추가

--- user-service/src/main/java/com/waither/userservice/common/config/RedisConfig.java
+++ user-service/src/main/java/com/waither/userservice/common/config/RedisConfig.java
@@ -3,14 +3,18 @@ package com.waither.userservice.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     @Bean
