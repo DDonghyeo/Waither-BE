d36ed57c9d9efc5d3d5982afe96652140091ab59
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-04-02 17:03:17 +0900

    :sparkles: ️feat: ApiGateway Config / Filter 설정

---
+++ apiGateway-service/src/main/java/com/waither/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,36 @@
+package com.waither.apigatewayservice.config;
+
+import com.waither.apigatewayservice.filter.CustomFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+
+        CustomFilter customFilter = new CustomFilter();
+
+        return builder.routes()
+                .route(r -> r.path("/user/**") //Path 확인
+                        .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config()))
+                        ) //필터 적용
+                        .uri("lb://USER-SERVICE") //uri로 이동
+                )
+                .route(r -> r.path("/user/actuator/**") //Actuator Path
+                        .and().method("POST", "GET") //Actuator Method : POST, GET
+                        .uri("lb://USER-SERVICE") //uri로 이동
+                )
+                .route(r -> r.path("/app/**")
+                        .filters(f -> f.addRequestHeader("App Request Header", "App Service Request")
+                                .addResponseHeader("App Response Header", "App Service Response")
+                        ) //필터 적용
+                        .uri("lb://APP-SERVICE")
+                )
+                .build();
+
+    }
+}
