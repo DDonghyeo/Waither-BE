c1d5d76b8b8375ef761c9e598232057603478229
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-04-04 21:41:13 +0900

    ♻️ refactor: SecurityConfig.java 수정

--- apiGateway-service/src/main/java/com/waither/apigatewayservice/filter/SecurityConfig.java
+++ apiGateway-service/src/main/java/com/waither/apigatewayservice/filter/SecurityConfig.java
@@ -3,17 +3,9 @@ package com.waither.apigatewayservice.filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration // IoC 빈(bean)을 등록
 @EnableWebFluxSecurity // 필터 체인 관리 시작 어노테이션
