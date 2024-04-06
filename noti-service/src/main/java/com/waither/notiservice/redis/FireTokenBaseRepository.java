ba01ef4d82a08e5cf4a2c1423338b51c2b905d91
DDonghyeo <gsu9045000@gmail.com>
2024-04-06 16:59:05 +0900

    💄style : @Repository

--- noti-service/src/main/java/com/waither/notiservice/redis/FireTokenBaseRepository.java
+++ noti-service/src/main/java/com/waither/notiservice/redis/FireTokenBaseRepository.java
@@ -2,6 +2,8 @@ package com.waither.notiservice.redis;
 
 import com.waither.notiservice.domain.FireBaseToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FireTokenBaseRepository extends CrudRepository<FireBaseToken, Long> {
 }
--- noti-service/src/main/java/com/waither/notiservice/repository/NotificationRepository.java
+++ noti-service/src/main/java/com/waither/notiservice/repository/NotificationRepository.java
@@ -2,9 +2,11 @@ package com.waither.notiservice.repository;
 
 import com.waither.notiservice.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, String> {
 
     List<Notification> findAllByUserId(Long userId);
