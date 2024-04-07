e4891eb5c2fb13c947b7eb4ade37bacb78939cff
seheonnn <ho78901@naver.com>
2024-04-07 22:11:54 +0900

    ✅  test: x, y 좌표 <-> 위도, 경도 변환 로직 테스트 코드 작성

---
+++ weather-service/src/test/java/com/waither/weatherservice/GpsTransferTest.java
@@ -0,0 +1,58 @@
+package com.waither.weatherservice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.waither.weatherservice.gps.GpsTransfer;
+import com.waither.weatherservice.gps.LatXLngY;
+
+@SpringBootTest
+class GpsTransferTest {
+
+	@Autowired
+	GpsTransfer gpsTransfer;
+
+	LatXLngY expected;
+
+	@BeforeEach
+	public void setUp() {
+		expected = LatXLngY.builder()
+			.x(55)
+			.y(127)
+			.lat(37.582301421185406)
+			.lng(126.69838413050432)
+			.build();
+	}
+
+	@Test
+	void convertGpsToGridTest() {
+
+		// given
+		double lat = 37.582301421185406;
+		double lng = 126.69838413050432;
+
+		// when
+		LatXLngY latXLngY = gpsTransfer.convertGpsToGrid(lat, lng);
+
+		// then
+		assertEquals(expected, latXLngY);
+	}
+
+	@Test
+	void convertGridToGps() {
+
+		// given
+		double x = 55;
+		double y = 127;
+
+		// when
+		LatXLngY latXLngY = gpsTransfer.convertGridToGps(x, y);
+
+		// then
+		assertEquals(expected, latXLngY);
+	}
+}
