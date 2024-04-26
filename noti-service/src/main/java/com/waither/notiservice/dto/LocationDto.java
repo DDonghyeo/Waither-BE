b27dc1779643bf34f2e64bbda1326ff45f91b7b3
DDonghyeo <gsu9045000@gmail.com>
2024-04-26 17:39:17 +0900

    ✨feat : 현재 위치 전송 API 설계

---
+++ noti-service/src/main/java/com/waither/notiservice/dto/LocationDto.java
@@ -0,0 +1,16 @@
+package com.waither.notiservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationDto {
+
+    public double x;
+
+    public double y;
+}
