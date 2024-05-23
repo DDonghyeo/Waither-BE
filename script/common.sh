22e601f3b9aa6b00e874df5532d72d6ad727f72f
DDonghyeo <gsu9045000@gmail.com>
2024-05-23 23:03:13 +0900

    📝docs : Zookeeper, Kafka 환경 추가

--- script/common.sh
+++ script/common.sh
@@ -1,6 +1,8 @@
 # 현재 실행중인 도커 컨테이너중 단어가 포함 컨테이너를 검색
 RUNNING_EUREKA=$(docker ps | grep eureka)
 RUNNING_CONFIG=$(docker ps | grep config)
+RUNNING_ZOOKEEPER=$(docker ps | grep zookeeper)
+RUNNING_KAFKA=$(docker ps | grep kafka)
 
 # Eureka 검색
 if [ -z "$RUNNING_EUREKA" ]; then
@@ -18,4 +20,20 @@ if [ -z "$RUNNING_CONFIG" ]; then
     sleep 5 #Config 실행 5초 대기
 else
     echo "Config Service is already running"
+fi
+
+# Zookeeper 검색
+if [ -z "$RUNNING_ZOOKEEPER" ]; then
+    echo "Starting Zookeeper ..."
+    docker compose -f /home/docker-compose.yml up -d zookeeper
+else
+    echo "Zookeeper is already running"
+fi
+
+# Kafka 검색
+if [ -z "$RUNNING_ZOOKEEPER" ]; then
+    echo "Starting Kafka ..."
+    docker compose -f /home/docker-compose.yml up -d kafka
+else
+    echo "Kafka is already running"
 fi
