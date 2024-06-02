e67a00891fc8f5d06a53ca711209881467f30de5
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-06-02 17:27:15 +0900

    ♻️ refactor: Map<Season, UserMedian> 변환 로직, toInitialData 메서드 내부로 이동

--- user-service/src/main/java/com/waither/userservice/service/commandService/UserService.java
+++ user-service/src/main/java/com/waither/userservice/service/commandService/UserService.java
@@ -89,7 +89,6 @@ public class UserService {
         newUser.setUserData(userDataList);
         newUser.setUserMedian(userMedianList);
 
-
         // 초기값 Kafka 전송
         KafkaDto.InitialDataDto initialDataDto = KafkaConverter.toInitialData(newUser, newSetting, userMedianList);
         kafkaService.sendInitialData(initialDataDto);
