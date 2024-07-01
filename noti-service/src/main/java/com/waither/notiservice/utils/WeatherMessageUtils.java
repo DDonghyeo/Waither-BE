1e2efd7a37d477e1c1c401fed8ff5237bc5510af
DDonghyeo <gsu9045000@gmail.com>
2024-07-01 15:36:39 +0900

    ♻️refactor : Weather Service 통신 환경 구축

--- noti-service/src/main/java/com/waither/notiservice/utils/WeatherMessageUtils.java
+++ noti-service/src/main/java/com/waither/notiservice/utils/WeatherMessageUtils.java
@@ -7,12 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.waither.notiservice.enums.Expressions.*;
 
 @RequiredArgsConstructor
 @Component
-public class TemperatureUtils {
+public class WeatherMessageUtils {
 
     public static Expressions[] winterExpressions = {WARM, GOOD, LITTLE_COLD, COLD, VERY_COLD };
     public static Expressions[] springAndAutumnExpressions = {HOT, LITTLE_HOT, GOOD, LITTLE_COLD, COLD};
@@ -59,5 +60,59 @@ public class TemperatureUtils {
         } else { //봄, 가을
             return springAndAutumnExpressions[level - 1].getExpression();
         }
+    }
+
+    public static String getRainPredictions(List<Double> predictions) {
+
+        List<String> predictionStr = predictions.stream()
+                .map(prediction -> prediction == 0 ? "강수없음" : getRainExpression(prediction))
+                .toList();
+        //예시 ["강수없음", "약한 비", "약한 비", "비", "비", "비"]
+
+        //몇 시간 뒤에 비가 얼만큼 몇 시간 동안 오는지?
+        int startHour = -1;
+        int duration = 0;
+        String intensity = "";
+        boolean isRaining = false;
+
+        for (int i = 0; i < predictionStr.size(); i++) {
+            String current = predictionStr.get(i);
+            if (!current.equals("강수없음")) {
+                if (!isRaining) {
+                    startHour = i + 1;
+                    intensity = current;
+                }
+                isRaining = true;
+                duration++;
+            } else if (isRaining) {
+                break;
+            }
+        }
+
+        if (startHour == -1) {
+            return null;
+        } else {
+            String timePhrase = startHour == 1 ? "1시간 후부터" : startHour + "시간 후부터";
+            String durationPhrase = duration == 1 ? "1시간 동안" : duration + "시간 동안";
+            //예시 "3시간 후부터 약한 비가 4시간 동안 올 예정입니다."
+            return String.format("%s %s가 %s 올 예정입니다.", timePhrase, intensity, durationPhrase);
+        }
+    }
+
+    //강수 표현
+    public static String getRainExpression(double prediction) {
+        //1~3mm : 약한 비
+        if (prediction > 1 && prediction < 3) {
+            return "약한 비";
+            //3~15mm : 비
+        } else if (prediction >=3 && prediction < 15) {
+            return "비";
+            //15~30mm 강한 비
+        } else if (prediction >= 15 &&prediction <= 30) {
+            return "강한 비";
+            //30mm~ 매우 강한 비
+        } else if (prediction >= 30) {
+            return "매우 강한 비";
+        } else return "비";
     }
 }
